68bf33e9ef0c58fa32491a29bf30d7c142f2cdf2
Inigo Meshoulam <ijmeshoulam@gmail.com>
2025-09-11 10:47:58 +0100

    Add keybindings for Telescope searches for: TODO/FIX/NOTE/BUG/HACK comments, and for modified files (according to Git) - both utilize Telescope.builtin functions.

--- init.lua
+++ init.lua
@@ -469,6 +469,11 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      vim.keymap.set('n', '<leader>sm', builtin.git_status, { desc = '[S]earch [M]odified Files (Git)' })
+      vim.keymap.set('n', '<leader>st', function()
+        builtin.grep_string { search = [[TODO:|FIX:|FIXME:|BUG:|HACK:|NOTE:]] }
+      end, { desc = '[S]earch [T]ODOs etc.' })
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
