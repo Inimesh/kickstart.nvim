e9522abc240c5f8628b8fba7d306e8085fde0a67
Inigo Meshoulam <ijmeshoulam@gmail.com>
2025-09-26 15:41:23 +0100

    Add autotag plugin; set guess-indent plugin to always load

---
+++ lua/custom/plugins/autotag.lua
@@ -0,0 +1,16 @@
+-- autotag
+-- https://github.com/windwp/nvim-ts-autotag
+--
+return {
+  'windwp/nvim-ts-autotag',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  opts = {
+    autotag = {
+      enable = true,
+      enable_rename = true,
+      enable_close = true,
+      enable_close_on_slash = true,
+      filetypes = { 'html', 'xml' },
+    },
+  },
+}
