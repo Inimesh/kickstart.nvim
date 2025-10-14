6a68c211020eafa7bcaa1a88cd0863cc58b29f2d
Inigo Meshoulam <ijmeshoulam@gmail.com>
2025-10-14 17:47:58 +0100

    Add window half and double height resize keymap

--- init.lua
+++ init.lua
@@ -242,6 +242,16 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<C-w>/', function()
+  local half_window_height = vim.api.nvim_win_get_height(0) / 2
+  vim.cmd('resize ' .. half_window_height)
+end, { desc = 'Half the window height' })
+
+vim.keymap.set('n', '<C-w>?', function()
+  local double_window_height = vim.api.nvim_win_get_height(0) * 2
+  vim.cmd('resize ' .. double_window_height)
+end, { desc = 'Double the window height' })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
