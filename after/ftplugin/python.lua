7d72adc3e2752007a7006827ea0cae6b4a89a83f
Inigo Meshoulam <ijmeshoulam@gmail.com>
2025-11-03 23:22:09 +0000

    Add filetype specific colorschemes

---
+++ after/ftplugin/python.lua
@@ -0,0 +1,5 @@
+local colorscheme = 'unokai'
+local ok, _ = pcall(vim.api.nvim_command, 'colorscheme ' .. colorscheme)
+if not ok then
+  print('error setting colorscheme ' .. colorscheme)
+end
---
+++ after/ftplugin/sh.lua
@@ -0,0 +1,5 @@
+local colorscheme = 'vim'
+local ok, _ = pcall(vim.api.nvim_command, 'colorscheme ' .. colorscheme)
+if not ok then
+  print('error setting colorscheme ' .. colorscheme)
+end
