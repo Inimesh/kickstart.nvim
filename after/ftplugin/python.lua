7e42102959dc0d013ea80aec544fd535c28b9ffb
Inigo Meshoulam <ijmeshoulam@gmail.com>
2025-12-11 17:25:24 +0000

    Add 80 column color line to buffer for python files

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -3,3 +3,7 @@ local ok, _ = pcall(vim.api.nvim_command, 'colorscheme ' .. colorscheme)
 if not ok then
   print('error setting colorscheme ' .. colorscheme)
 end
+ok, _ = pcall(vim.api.nvim_command, 'set colorcolumn=89')
+if not ok then
+  print 'error setting 80 column line'
+end
