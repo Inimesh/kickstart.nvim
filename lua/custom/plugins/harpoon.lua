a1469e28413dbce487895b97daeeafc51296dcf0
Inimesh <inigo.meshoulam@essentia-analytics.com>
2026-02-02 19:00:01 +0000

    Add harpoon

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,16 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local harpoon = require 'harpoon'
+    harpoon:setup()
+    vim.keymap.set('n', '<leader>a', function()
+      harpoon:list():add()
+    end, { desc = '[A]dd harpoon' })
+
+    vim.keymap.set('n', '<leader>b', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'Harpoon menu' })
+  end,
+}
