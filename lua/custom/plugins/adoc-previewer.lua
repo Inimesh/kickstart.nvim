51041310d902b171472ab4a7684e8222850d2ce5
Inimesh <inigo.meshoulam@essentia-analytics.com>
2026-03-13 09:19:33 +0000

    Comment commands for asciidoc preview plugin in file

--- lua/custom/plugins/adoc-previewer.lua
+++ lua/custom/plugins/adoc-previewer.lua
@@ -8,3 +8,8 @@ return {
     -- Add user configuration here
   },
 }
+-- Command Description
+-- :AsciiDocPreview: Starts the AsciiDoc preview server in background and opens the current AsciiDoc file in the standard web browser.
+-- :AsciiDocPreviewStop: (if needed) Stops the AsciiDoc preview server.
+-- :AsciiDocPreviewOpen: (if needed) (Re)Opens the current AsciiDoc file in the standard web browser.
+-- :AsciiDocPreviewNotify: (if needed) Notifies the server to display the current AsciiDoc file in the preview.
